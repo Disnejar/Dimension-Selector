aa91ee4e70dd25df8933f5e9b8a56deac23becca
Disnejar <filip.t.orlowski@gmail.com>
2022-04-08 12:34:29 +0200

    Added selecting bigger or smaller objects

--- DimensionSelector.py
+++ DimensionSelector.py
@@ -2,7 +2,7 @@ bl_info = {
     'name': 'Dimension Selector',
     'blender': (3, 1, 2),
     'category': 'Tools',
-    'version': (1, 0, 0),
+    'version': (1, 1, 3),
     'author': 'Disnejar',
     'description': 'A simple add on for selecting all objects with the same dimensions',
 }
@@ -32,6 +32,39 @@ class DIMENSIONSELTOOL_OT_select(bpy.types.Operator):
             if round(obj.dimensions.x, 3) == round(new_dim[0], 3) and round(obj.dimensions.y, 3) == round(new_dim[1], 3) and round(obj.dimensions.z, 3) == round(new_dim[2], 3):
                 obj.select_set(True)
                 context.view_layer.objects.active = obj
+            else: 
+                if round(obj.dimensions.x, 3) == round(new_dim[0], 3) and round(obj.dimensions.y, 3) == round(new_dim[2], 3) and round(obj.dimensions.z, 3) == round(new_dim[1], 3):
+                    obj.select_set(True)
+                    context.view_layer.objects.active = obj
+                else:
+                    if round(obj.dimensions.x, 3) == round(new_dim[2], 3) and round(obj.dimensions.y, 3) == round(new_dim[1], 3) and round(obj.dimensions.z, 3) == round(new_dim[0], 3):
+                        obj.select_set(True)
+                        context.view_layer.objects.active = obj
+                    else:
+                        if round(obj.dimensions.x, 3) == round(new_dim[1], 3) and round(obj.dimensions.y, 3) == round(new_dim[0], 3) and round(obj.dimensions.z, 3) == round(new_dim[2], 3):
+                            obj.select_set(True)
+                            context.view_layer.objects.active = obj
+                            
+            if True:#context.scene.smaller:
+                if round(obj.dimensions.x, 3) <= round(new_dim[0], 3) and round(obj.dimensions.y, 3) <= round(new_dim[1], 3) and round(obj.dimensions.z, 3) <= round(new_dim[2], 3):
+                    if context.scene.allSmaller:
+                        obj.select_set(True)
+                        context.view_layer.objects.active = obj
+                    else:
+                        if round(obj.dimensions.x, 3) >= round(new_dim[0], 3) * float(context.scene.smallerPercentage) / 100.0 and round(obj.dimensions.y, 3) >= round(new_dim[0], 3) * float(context.scene.smallerPercentage) / 100.0 and round(obj.dimensions.z, 3) >= round(new_dim[0], 3) * float(context.scene.smallerPercentage) / 100.0:
+                            obj.select_set(True)
+                            context.view_layer.objects.active = obj
+                    
+            if True:#context.scene.bigger:
+                if round(obj.dimensions.x, 3) >= round(new_dim[0], 3) and round(obj.dimensions.y, 3) >= round(new_dim[1], 3) and round(obj.dimensions.z, 3) >= round(new_dim[2], 3):
+                    if context.scene.allBigger:
+                        obj.select_set(True)
+                        context.view_layer.objects.active = obj
+                    else:
+                        if round(obj.dimensions.x, 3) <= round(new_dim[0], 3) * float(context.scene.biggerPercentage) / 100.0 and round(obj.dimensions.y, 3) <= round(new_dim[0], 3) * float(context.scene.biggerPercentage) / 100.0 and round(obj.dimensions.z, 3) <= round(new_dim[0], 3) * float(context.scene.biggerPercentage) / 100.0:
+                            obj.select_set(True)
+                            context.view_layer.objects.active = obj
+                        
 
                 
         return {'FINISHED'}
@@ -51,53 +84,84 @@ class DIMENSIONSELTOOL_OT_use_active(bpy.types.Operator):
             context.scene.Z = context.active_object.dimensions.z
                 
         return {'FINISHED'}
-    
-
-class DIMENSIONSELTOOL_PT_panel(bpy.types.Panel):
-    bl_idname = "DIMENSIONSELTOOL_PT_panel"
-    bl_label = "Dimension Selector"
+        
+        
+class DIMENSIONSELTOOL_PT_dimensions_panel(bpy.types.Panel):
+    bl_idname = "DIMENSIONSELTOOL_PT_dimensions_panel"
+    bl_label = "Dimensions"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = "Tool"
-
+    bl_category = "Dimensional Select"
+    
     def draw(self, context):
-        
-        box = self.layout.box()
-        
-        row = box.row()
-        row.label(text = 'Dimensions')
             
-        col = box.column(align = True)
+        col = self.layout.column(align = True)
         col.prop(context.scene, "X")
         col.prop(context.scene, "Y")
         col.prop(context.scene, "Z")
-            
-        row = box.row()
+        
+        row = self.layout.row()
         row.operator('dimensionseltool.use_active')
         
-        row = box.row()
-        row.prop(context.scene, "add")
+class DIMENSIONSELTOOL_PT_selection_panel(bpy.types.Panel):
+    bl_idname = "DIMENSIONSELTOOL_PT_selection_panel"
+    bl_label = "Selection Options"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Dimensional Select"
+    
+    def draw(self, context):
+            
+        row = self.layout.row()
+        row.prop(context.scene, 'add')
         
+        
+        #row = self.layout.row()
+        #row.prop(context.scene, 'bigger')
+        
+        #if context.scene.bigger:
+        row = self.layout.row()
+        row.prop(context.scene, 'allBigger')
+            
+            #if not context.scene.allBigger:
+                #row = self.layout.row()
+                #row.prop(context.scene, 'biggerPercentage')
+                
+                
+        #row = self.layout.row()
+        #row.prop(context.scene, 'smaller')
+        
+        #if context.scene.smaller:
+        row = self.layout.row()
+        row.prop(context.scene, 'allSmaller')
+            
+            #if not context.scene.allSmaller:
+                #row = self.layout.row()
+                #row.prop(context.scene, 'smallerPercentage')
+                
         row = self.layout.row()
         row.operator('dimensionseltool.select')
         
-        #self.layout.row.prop(context.scene.boundaryseltool.select.my_dim, "Dimensions")
-        #props = self.layout.operator('boundaryseltool.select')
-
 
 
 PROPS = [
-    ('X', bpy.props.FloatProperty(name='X', default=0, precision = 3)),
-    ('Y', bpy.props.FloatProperty(name='Y', default=0, precision = 3)),
-    ('Z', bpy.props.FloatProperty(name='Z', default=0, precision = 3)),
+    ('X', bpy.props.FloatProperty(name='X', default=0, precision = 3, unit = 'LENGTH', subtype = 'DISTANCE')),
+    ('Y', bpy.props.FloatProperty(name='Y', default=0, precision = 3, unit = 'LENGTH', subtype = 'DISTANCE')),
+    ('Z', bpy.props.FloatProperty(name='Z', default=0, precision = 3, unit = 'LENGTH', subtype = 'DISTANCE')),
     ('add', bpy.props.BoolProperty(name='Add To Selection', default=False)),
+    ('bigger', bpy.props.BoolProperty(name='Select Bigger Objects', default=True)),
+    ('smaller', bpy.props.BoolProperty(name='Select Smaller Objects', default=True)),
+    ('allSmaller', bpy.props.BoolProperty(name='Select All Smaller Objects', default=False)),
+    ('allBigger', bpy.props.BoolProperty(name='Select All Bigger Objects', default=False)),
+    ('biggerPercentage', bpy.props.IntProperty(name='Percantage', default=150, soft_max = 500, min = 100, soft_min = 100, subtype = 'PERCENTAGE')),
+    ('smallerPercentage', bpy.props.IntProperty(name='Percantage', default=50, soft_max = 500, min = 0, subtype = 'PERCENTAGE')),
 ]
-
 
 CLASSES = [
     DIMENSIONSELTOOL_OT_select,
     DIMENSIONSELTOOL_OT_use_active,
-    DIMENSIONSELTOOL_PT_panel
+    DIMENSIONSELTOOL_PT_dimensions_panel,
+    DIMENSIONSELTOOL_PT_selection_panel
 ]
 
 
@@ -107,6 +171,7 @@ def register():
     
     for klass in CLASSES:
         bpy.utils.register_class(klass)
+
 
 def unregister():
     for klass in CLASSES:
